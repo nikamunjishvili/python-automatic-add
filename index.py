28c584216219bdc8b18df0ebe562d918b1ad4ebf
Nika Munjishvili <nikamunja31415@gmail.com>
2023-01-28 13:06:42 +0400

    code review

--- index.py
+++ index.py
@@ -1,18 +1,45 @@
 fruits = {}
 
+
+def end_questions():
+    ques = input("Add More? yes/no: ")
+    ques = ques.lower()
+    if ques == "yes":
+        return True
+    else:
+        return False
+
+
+def main_question():
+    helper = {}
+    name = input("enter fruit name: ")
+    kg = int(input("input fruits weight: "))
+    price = input("Enter fruit price: ")
+    basket = {}
+    helper["kg"] = kg
+    helper["price"] = helper
+    fruits[name] = helper
+
+    return fruits
+
+
 while True:
     if len(fruits) == 0:
-        name = input("enter fruit name: ")
-        kg = int(input("input fruits weight: "))
-        fruits[name] = int(kg)
+        main_question()
     else:
         print("this is your cart: ", fruits)
-        ques = input("Add More? yes/no: ")
-        if ques == "yes":
-            name = input("enter fruit name: ")
-            kg = int(input("input fruits weight: "))
-            fruits[name] = int(kg)
+
+        if end_questions == True:
+            main_question()
         else:
             break
+result = 0
 
-print("this is your cart: ", fruits)
+for key, value in fruits.items():
+    kg = int(value['kg'])
+    price = int(value['price'])
+    result += kg*price
+    print(key, "your amount", kg*price, "GEL")
+
+
+print("you have to pay: ", result, "GEL")
